16889fd6d59806c7cb161f3a2b6e7668ce0c61be
Meet Pandya <pandyama2@gmail.com>
2020-12-19 20:36:50 -0500

    Added changes for LinkedList
    Created a Node class and a main class

---
+++ src/LinkedList/main.java
@@ -0,0 +1,25 @@
+package LinkedList;
+
+public class main {
+    public static void main(String[] args){
+        Node linkedlist = new Node(5);
+        Node n2 = new Node(10);
+        Node n3 = new Node(15);
+
+        linkedlist.next = n2;
+        n2.next = n3;
+
+        printNode(linkedlist);
+    }
+
+    public static void printNode(Node n1){
+        while(n1.next != null){
+            System.out.println(n1.data);
+            n1 = n1.next;
+            if(n1.next == null){
+                System.out.println(n1.data);
+                break;
+            }
+        }
+    }
+}
